bc36de0bb1cb9c883d6b930d319ab182644774ee
Thomas @ BeeDesk <thomas@beedesk.com>
2012-12-22 23:49:19 -0800

    Added a timeout mechanism to warn un-notified listener.

--- after.js
+++ after.js
@@ -1,7 +1,37 @@
 ;(function() {
+    var DEBUG = true;
+    var DEFAULT_TIMEOUT = 30 * 1000;
+
     function main(_) {
         var instance;
-    
+        var timeout = null;
+
+        function touchTimer() {
+            !timeout || clearTimeout(timeout);
+            timeout = setTimeout(function() {
+                dumpStatus();
+            }, DEFAULT_TIMEOUT);
+        }
+        
+        function stopTimer() {
+            !timeout || clearTimeout(timeout);
+            timeout = null;
+        }
+        
+        function dumpStatus() {
+            var msg = 'There is one or more listener who has not be notified. ';
+            msg += 'for at least ' + DEFAULT_TIMEOUT + 'ms. \n';
+  
+            if ('JSON' in window) {
+                 msg += 'Listeners: ' + JSON.stringify(instance.binders) + '\n';
+                 msg += 'Happned events: ' + JSON.stringify(instance.events);
+            } else {
+                 msg += 'We wanted to tell you which one, but your browser does ';
+                 msg += 'not support JSON.';
+            }
+            DEBUG && console.warn(msg);
+        }
+
         function after(eventnames, callback) {
             var intersect = _.intersection(instance.events, eventnames);
 
@@ -12,6 +42,7 @@
                     events: eventnames.slice(0),
                     fn: callback
                 });
+                touchTimer();
             }
         }
 
@@ -35,11 +66,17 @@
                     var binder = matched[i];
                     binder.fn();
                 }
+                
+                if (instance.binders.length > 0) {
+                    touchTimer();
+                } else {
+                    stopTimer();
+                }
             } else {
                 console.warn('Repeated event, "' + name + '", ignored.');
             }
         }
-        
+
         function reset() {
           instance = {events: [], binders: []};
         }
@@ -48,8 +85,8 @@
         return {after: after, happen: happen, reset: reset};
     }
 
-    // handle requirejs
     if (typeof define === 'function' && define.amd) {
+        // handle requirejs
         define('after', ['underscore'], function(underscore) {
             return function() {
               return main(_);
